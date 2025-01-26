61fdc7a52064465e865f7bfa714c6550787ca8c0
Kirill Lysin <lysinkirill@gmail.com>
2025-01-26 23:36:48 +0300

    [REACT-REDUX]: add user details and user details page components

---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectUserById } from '../features/users/selectors';
+import { updateUserName } from '../features/users/usersSlice';
+
+const UserDetails: React.FC = () => {
+    const { id } = useParams<{ id: string }>();
+    const userId = parseInt(id || '0');
+    const user = useSelector(selectUserById(userId));
+    const dispatch = useDispatch();
+
+    const [name, setName] = useState(user?.name || '');
+
+    const handleUpdate = () => {
+        if (user) {
+            dispatch(updateUserName({ id: user.id, name }));
+        }
+    };
+
+    if (!user) {
+        return <p>User not found</p>;
+    }
+
+    return (
+        <div>
+            <h1>Update User</h1>
+            <p>ID: {user.id}</p>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <button onClick={handleUpdate}>Update</button>
+        </div>
+    );
+};
+
+export default UserDetails;
